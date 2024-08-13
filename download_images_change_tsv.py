2955116ec69dc658d29d84c9a569d01cc47bdf09
Nicholas Mesa-Cucalon <nmesacuc@andrew.cmu.edu>
2024-08-13 16:38:56 -0400

    Updated image download script to also create a new tsv file for use

--- download_images_change_tsv.py
+++ download_images_change_tsv.py
@@ -1,5 +1,5 @@
 """
-A Script that iterates through a dataset and downloads all the associated image files
+A Script that iterates through a dataset, downloads all the associated image files locally and then updates tsv files to point to these local copies
 """
 
 import pandas as pd
@@ -7,19 +7,20 @@ import requests
 import os
 from urllib.parse import urlparse
 
-# 0. Helper function
-def download_and_save_image(url, directory):
+# 0. Helper functions
+def download_image(url, img_dir):
     # Create the directory if it doesn't exist
-    os.makedirs(directory, exist_ok=True)
+    os.makedirs(img_dir, exist_ok=True)
 
     # Get the filename from the URL
     filename = os.path.basename(urlparse(url).path)
 
     # Full path for the image
-    filepath = os.path.join(directory, filename)
+    filepath = os.path.join(img_dir, filename)
     if ('jpg' not in filepath) and ('png' not in filepath) and ('webp' not in filepath) and ('jpeg' not in filepath):
         print(f"{filepath} does not have any file extension!")
         filepath += '.jpg'
+        filename += '.jpg'
 
     # Download the image
     response = requests.get(url)
@@ -32,6 +33,7 @@ def download_and_save_image(url, directory):
         print(f"Image downloaded successfully: {filepath}")
     else:
         print(f"Failed to download image. Status code: {response.status_code}")
+    return filename
 
 # 0. Setup
 training_path     = './datasets/scizor_train.tsv'
@@ -46,11 +48,30 @@ val_df            = pd.read_table(val_path, delimiter='\t')
 train_urls        = train_df.iloc[:,1].to_list()
 val_urls          = val_df.iloc[:,1].to_list()
 
-# 1. Iterate
-for url in train_urls:
-    download_and_save_image(url, training_img_path)
+train_names       = []
+val_names         = []
 
-for url in val_urls:
-    download_and_save_image(url, val_img_path)
+# 1. Download all images and collect all filenames
+for i in range(len(train_urls)):
+    filename = download_image(train_urls[i],training_img_path)
+    train_names.append(filename)
 
-print("Done!")
+for i in range(len(val_urls)):
+    filename = download_image(val_urls[i],training_img_path)
+    val_names.append(filename)
+
+# 2. Open the new tsv files
+local_train_df      = pd.read_table(training_path, delimiter='\t')
+local_val_df        = pd.read_table(val_path, delimiter='\t')
+
+# 3. Create dataframes from the lists
+train_col = pd.DataFrame(train_names)
+val_col   = pd.DataFrame(val_names)
+
+# 4. Update the local dataframes
+local_train_df.image = train_col
+local_val_df.image = val_col
+
+# 5. Save new tsv files for local use
+local_train_df.to_csv('./datasets/scizor_train_local.tsv', sep="\t")
+local_val_df.to_csv('./datasets/scizor_val_local.tsv', sep="\t")
