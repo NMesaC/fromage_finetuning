d60005504b8f85a3a4ef2c0b9067b506eabda2f2
Nicholas Mesa-Cucalon <40221378+NMesaC@users.noreply.github.com>
2024-08-17 21:05:49 -0400

    Update scraper.py

--- webscraper/scraper.py
+++ webscraper/scraper.py
@@ -1,22 +1,22 @@
-# import json
-# from serpapi.google_search import GoogleSearch
+import json
+from serpapi.google_search import GoogleSearch
 
-# api_key = "meow"
+# Running this script will correctly scrape google images into a json file, but it will consume a search with the API key
 
-# params = {
-#   "q": "Scizor",
-#   "engine": "google_images",
-#   "ijn": "0",
-#   "safe": "active",
-#   "nfpr": "1",
-#   "api_key": api_key
-# }
+api_key = "meow"
 
-# with open("scizor_res.json","w") as f:
-#     search = GoogleSearch(params)
-#     results = search.get_dict()
-#     images_results = results["images_results"]
-#     f.write(json.dumps(images_results))
+params = {
+  "q": "Scizor",
+  "engine": "google_images",
+  "ijn": "0",
+  "safe": "active",
+  "nfpr": "1",
+  "api_key": api_key
+}
 
+with open("scizor_res.json","w") as f:
+    search = GoogleSearch(params)
+    results = search.get_dict()
+    images_results = results["images_results"]
+    f.write(json.dumps(images_results))
 
-# RUNNING THIS WORKS BUT CONSUMES A SEARCH!!!!
